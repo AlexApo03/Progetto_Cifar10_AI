e5318f2d889b88528a64b3fc57169c8a5f9966c8
AlexApo03 <alexandruapostol@studio.unibo.it>
2024-12-03 14:49:45 +0100

    Revisione import

--- model.py
+++ model.py
@@ -1,6 +1,7 @@
 import torchvision.models as models
 import torch.nn as nn
 
+# Architettura utilizzata
 def get_resnet_model(num_classes):
     model = models.resnet18(weights='DEFAULT')  
     model.fc = nn.Linear(model.fc.in_features, num_classes)
